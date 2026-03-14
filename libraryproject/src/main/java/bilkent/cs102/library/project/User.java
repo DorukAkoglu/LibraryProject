7de9197ceb5f1b0bfa3a14b2cbbbc7fdff98ef34
Doruk Akoğlu <dorukakoglu@gmail.com>
2026-03-14 14:48:40 +0300

    added instance variables to User class

--- libraryproject/src/main/java/bilkent/cs102/library/project/User.java
+++ libraryproject/src/main/java/bilkent/cs102/library/project/User.java
@@ -1,5 +1,10 @@
 package bilkent.cs102.library.project;
 
 public class User {
-    
+    private int userId;
+    private String name;
+    private String email;
+    private String password;
+    private List<String> courses;
+    private String availability;
 }
