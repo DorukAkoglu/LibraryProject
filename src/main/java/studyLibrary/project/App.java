f44c3b74acd46c062eecc4be0bb78f14d0c729aa
Doruk Akoğlu <dorukakoglu@gmail.com>
2026-03-16 01:16:32 +0300

    changed version of maven

--- src/main/java/studyLibrary/project/App.java
+++ src/main/java/studyLibrary/project/App.java
@@ -1,12 +1,17 @@
 package studyLibrary.project;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
 public class App extends Application
 {
     public void start(Stage stage){
+        Group root = new Group();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
         stage.show();
     }
     
