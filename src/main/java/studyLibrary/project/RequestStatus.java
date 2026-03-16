c7599c3571710bc656b6247e2718ab73020dbb70
Doruk Akoğlu <dorukakoglu@gmail.com>
2026-03-16 19:13:13 +0300

    some implementation of studymate classes

---
+++ src/main/java/studyLibrary/project/RequestStatus.java
@@ -0,0 +1,7 @@
+package studyLibrary.project;
+
+public enum RequestStatus {
+    PENDING,
+    ACCEPTED,
+    REJECTED
+}
--- src/main/java/studyLibrary/project/StudyRequest.java
+++ src/main/java/studyLibrary/project/StudyRequest.java
@@ -1,13 +1,37 @@
 package studyLibrary.project;
 
+import java.time.LocalDateTime;
+
 public class StudyRequest {
     private String sender;
     private String receiver;
-    private String status; // "pending", "accepted", "rejected"
+    private RequestStatus status; // PENDING, ACCEPTED, REJECTED (ENUM)
+    private LocalDateTime timestamp;
     public StudyRequest(String sender, String receiver) {
         this.sender = sender;
         this.receiver = receiver;
-        this.status = "pending";
+        this.status = RequestStatus.PENDING;
+        timestamp = LocalDateTime.now();
     }
-    
+    public void acceptRequest() {
+        this.status = RequestStatus.ACCEPTED;
+    }
+    public void rejectRequest() {
+        this.status = RequestStatus.REJECTED;
+    }
+    public String getSender() {
+        return sender;
+    }
+    public String getReceiver() {
+        return receiver;
+    }
+    public RequestStatus getStatus() {
+        return status;
+    }
+    public void setStatus(RequestStatus status) {
+        this.status = status;
+    }
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    } 
 }
