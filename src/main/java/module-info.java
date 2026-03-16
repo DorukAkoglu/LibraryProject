dad0b2447427aab0aa054d50d8a575e0df02476c
Doruk Akoğlu <dorukakoglu@gmail.com>
2026-03-16 17:39:37 +0300

    changed maven version to 17 to get rid of warnings and some technical changes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module studyLibrary.project {
     requires javafx.controls;
+    requires javafx.fxml;
     requires transitive javafx.graphics;
-    opens studyLibrary.project to javafx.graphics, javafx.fxml, javafx.controls;
+
+    opens studyLibrary.project to javafx.fxml, javafx.graphics;
     exports studyLibrary.project;
 }
