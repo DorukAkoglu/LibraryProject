e3be15fb9660376ea01a96a97444e12e199cb286
Mert Efe Tezer <tezermertefe@gmail.com>
2026-03-17 04:08:32 +0300

    Librarian and admin templates

---
+++ src/main/java/studyLibrary/project/Admin.java
@@ -0,0 +1,20 @@
+package studyLibrary.project;
+
+public class Admin extends User{
+
+    public Admin(int userID, String name, String email, String password) {
+        super(userID, name, email, password);
+    }
+    public void addUser(User u){
+        //TODO
+    }
+    public void removeUser(User u){
+        //TODO
+    }
+    public void generateReport(User u){
+        //TODO
+    }
+}
+
+
+
