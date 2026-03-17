c26c87b988904e9303a14dd6f96b20738e796517
Mehmet Samed Tek <mehmetsamedtek@gmail.com>
2026-03-17 03:04:56 +0300

    changing structure of LibrarySystem and adding MainController class without any method for now.

--- src/main/java/studyLibrary/project/LibrarySystem.java
+++ src/main/java/studyLibrary/project/LibrarySystem.java
@@ -1,23 +1,36 @@
-    private DatabaseManager db;
 package studyLibrary.project;
-
+import java.util.ArrayList;
 import java.util.List;
+import javafx.scene.CacheHint;
 
 public class LibrarySystem {
-    
+    private DatabaseManager db;
     private List<StudyRequest> requests;
     private List<StudyMatch> matches;
-    private List<Student> students;
+    private List<User> users;
+    private List <Book> books;
+    private List <Book> borrowedBooks;
+    private List <Student> activeStudents;
+    private List <Message> chats;
 
-    public LibrarySystem(List<StudyRequest> requests, List<StudyMatch> matches, List<Student> students) {
-        this.requests = requests;
-        this.matches = matches;
-        this.students = students;
+
+    public LibrarySystem() {
+        requests = new ArrayList<StudyRequest>();
+        matches = new ArrayList<StudyMatch>();
+        books = new ArrayList<Book>();
+        activeStudents = new ArrayList<Student>();
+        users = new ArrayList<User>();
+        borrowedBooks = new ArrayList<Book>();
+        db = new DatabaseManager();
+        chats = new ArrayList<Message>();
+        
     }
-    //StudyMate kısmı için yapmam gerekti
+    
     public void acceptRequest(StudyRequest request){
         StudyMatch studyMatch = new StudyMatch(request.getSender(), request.getReceiver());
         studyMatch.setCourse(request.getCourse());
+        this.activeStudents.remove(request.getSender());
+        this.activeStudents.remove(request.getReceiver());
         this.matches.add(studyMatch);
         this.requests.remove(request);
     }
