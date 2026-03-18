9d7a51c4a00b71dd2ff79b17654d5cabbd095199
Berke Güler <fberke.dr1907@gmail.com>
2026-03-18 13:39:09 +0300

    adding  methods

--- src/main/java/studyLibrary/project/Comment.java
+++ src/main/java/studyLibrary/project/Comment.java
@@ -4,11 +4,34 @@ public class Comment {
     private User user;
     private String content;
 
+    public Comment(User user, String content){
+        this.user = user;
+        this.content = content;
+    }
+
     public boolean editComment(String com){
-        return false;
+        this.content = com;
+        return true;
     }
 
     public boolean deleteComment(){
+        //TODO
         return false;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
     }
 }
--- src/main/java/studyLibrary/project/Review.java
+++ src/main/java/studyLibrary/project/Review.java
@@ -1,5 +1,6 @@
 package studyLibrary.project;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Review {
@@ -9,15 +10,67 @@ public class Review {
     private List<Comment> comments;
     private int rating;
 
+    public Review(User user, Book book, String content, int rating){
+        this.user = user;
+        this.book = book;
+        this.content = content;
+        this.rating = rating;
+        this.comments = new ArrayList<>();
+    }
+
     public boolean editReview(String s, int rating){
-        return false;
+        this.content = s;
+        this.rating = rating;
+        return true;
     }
 
     public boolean deleteReview(){
+        //TODO
         return false;
     }
 
     public Comment addComment(Review r){
+        //TODO
         return null;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void addCommentToList(Comment comment) {
+        this.comments.add(comment);
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
     }
 }
