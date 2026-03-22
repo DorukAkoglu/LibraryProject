76519cf1cb1fef25ddd414e398c4bb133a401ebd
Berke Güler <fberke.dr1907@gmail.com>
2026-03-23 01:10:23 +0300

    adding some methods

--- src/main/java/studyLibrary/project/BookController.java
+++ src/main/java/studyLibrary/project/BookController.java
@@ -1,26 +1,96 @@
 package studyLibrary.project;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
 public class BookController {
+    @FXML private TextField searchField;
+    @FXML private ListView<Book> bookListView;
+    @FXML private Button borrowButton;
+    @FXML private Button returnButton;
+
     private LibrarySystem system;
+    private static final int BORROW_DURATION = 14;
+
+    private User currentUser;
+
+    @FXML
+    public void initialize(){
+        this.system = LibrarySystem.getInstance();
+
+    }
+
+    @FXML
+    public void handleSearchAction(){
+        String searchWord = searchField.getText();
+        List<Book> results = searchBook(searchWord);
+        
+        // Bulunan kitapları ekrandaki listeye (ListView) bas
+        bookListView.getItems().clear();
+        bookListView.getItems().addAll(results);
+    }
+
+    private void showAlert(String baslik, String mesaj, Alert.AlertType tip) {
+        Alert alert = new Alert(tip);
+        alert.setTitle(baslik);
+        alert.setHeaderText(null);
+        alert.setContentText(mesaj);
+        alert.showAndWait();
+    }
+
 
     public List<Book> searchBook(String title){
-        return null;
+        List<Book> foundBooks = new ArrayList<>();
+
+        for (Book book : system.getBooks()){
+            if (book.getTitle().toLowerCase().contains(title.toLowerCase())) {
+                foundBooks.add(book); 
+            }
+        }
+        return foundBooks;
     }
-    public boolean borrowBook(User u, Book b){
+    public boolean borrowBook(User u, Book book){
+        if (book.isAvailable()) {
+            book.setNumCopies(book.getNumCopies() - 1);
+            book.setDueTime(LocalDate.now().plusDays(BORROW_DURATION));
+            
+            if (!system.getBorrowedBooks().contains(book)) {
+                system.getBorrowedBooks().add(book);
+            }
+            system.updateBook(book);
+            return true;
+        }
         return false;
     }
 
     public boolean returnBook(User u, Book b){
-        return false;
+        b.setNumCopies(b.getNumCopies() + 1);
+        b.setDueTime(null);
+        system.getBorrowedBooks().remove(b);
+        system.updateBook(b);
+        return true;
     }
 
     public boolean extendBook(User u, Book b){
+        if (b.getDueTime() != null) {
+            b.setDueTime(b.getDueTime().plusDays(7));
+            system.updateBook(b); 
+            return true;
+        }
         return false;
     }
 
     public boolean reserveBook(User u, Book b){
+        if (!b.isAvailable()) {
+            return true;
+        }
         return false;
     }
 }
