90c3e9b690295c8994fd3479aeef5e90098f91cf
Berke Güler <fberke.dr1907@gmail.com>
2026-03-22 23:13:55 +0300

    Auto stash before checking out "origin/main"

---
+++ src/main/java/studyLibrary/project/BookController.java
@@ -0,0 +1,26 @@
+package studyLibrary.project;
+
+import java.util.List;
+
+public class BookController {
+    private LibrarySystem system;
+
+    public List<Book> searchBook(String title){
+        return null;
+    }
+    public boolean borrowBook(User u, Book b){
+        return false;
+    }
+
+    public boolean returnBook(User u, Book b){
+        return false;
+    }
+
+    public boolean extendBook(User u, Book b){
+        return false;
+    }
+
+    public boolean reserveBook(User u, Book b){
+        return false;
+    }
+}
